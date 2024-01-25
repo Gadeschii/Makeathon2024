43a4529ff4cebd92958681e9e682d347f2a2b3db
Gadeschii <javier.izquierdo.102.ulpgc@gmail.com>
2024-01-25 12:15:34 +0000

    Encrypted MongoDb password

--- config/properties.js
+++ config/properties.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
+
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: 'mongodb+srv://Makeathon24:qwerty123456@cluster0.bgfdccw.mongodb.net/Makeathon24'
+    DB: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bgfdccw.mongodb.net/Makeathon24`
 }
